46d3c9c360f1b201fb56a5d3ea9f4e514d5dc864
Dominik Schulz <dominikschulz@gmx.net>
2022-07-24 15:35:31 +0200

    Added proper colors and TextTheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:vocabulary/providers/page_index_provider.dart';
 import 'package:vocabulary/styles/color_schemes.g.dart';
+import 'package:vocabulary/styles/custom_color.g.dart';
 import 'package:vocabulary/wrapper/page_wrapper.dart';
 
 void main() {
@@ -16,120 +18,142 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Vocabulary',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: lightColorScheme,
-        textTheme: Theme.of(context).textTheme.copyWith(
-              displaySmall: const TextStyle(
-                fontFamily: "Catamaran",
-                fontSize: 28,
-                fontWeight: FontWeight.w700,
-                //height: 44,
-              ),
-              headlineMedium: const TextStyle(
-                fontFamily: "Catamaran",
-                fontSize: 28,
-                fontWeight: FontWeight.w700,
-                //height: 36,
-              ),
-              headlineSmall: const TextStyle(
-                fontFamily: "Catamaran",
-                fontSize: 24,
-                fontWeight: FontWeight.w700,
-                //height: 32,
-              ),
-              titleLarge: const TextStyle(
-                fontFamily: "Catamaran",
-                fontWeight: FontWeight.w600,
-                fontSize: 22,
-                //height: 28,
-              ),
-              labelLarge: const TextStyle(
-                fontFamily: "Noto Sans",
-                fontWeight: FontWeight.w600,
-                fontSize: 14,
-                //Commented out because of occuring bug
-                //height: 20,
-              ),
-              bodyLarge: const TextStyle(
-                fontFamily: "Noto Sans",
-                fontWeight: FontWeight.w500,
-                fontSize: 16,
-                //height: 20,
-              ),
-              bodyMedium: const TextStyle(
-                fontFamily: "Noto Sans",
-                fontWeight: FontWeight.w400,
-                fontSize: 14,
-                //height: 20,
-              ),
-              bodySmall: const TextStyle(
-                fontFamily: "Noto Sans",
-                fontWeight: FontWeight.w400,
-                fontSize: 12,
-                //height: 16,
-              ),
-            ),
-      ),
-      darkTheme: ThemeData(
-        useMaterial3: true,
-        colorScheme: darkColorScheme,
-        textTheme: Theme.of(context).textTheme.copyWith(
-              displaySmall: const TextStyle(
-                fontFamily: "Catamaran",
-                fontSize: 28,
-                fontWeight: FontWeight.w700,
-                //height: 44,
-              ),
-              headlineMedium: const TextStyle(
-                fontFamily: "Catamaran",
-                fontSize: 28,
-                fontWeight: FontWeight.w700,
-                //height: 36,
-              ),
-              headlineSmall: const TextStyle(
-                fontFamily: "Catamaran",
-                fontSize: 24,
-                fontWeight: FontWeight.w700,
-                //height: 32,
-              ),
-              titleLarge: const TextStyle(
-                fontFamily: "Catamaran",
-                fontWeight: FontWeight.w600,
-                fontSize: 22,
-                //height: 28,
-              ),
-              labelLarge: const TextStyle(
-                fontFamily: "Noto Sans",
-                fontWeight: FontWeight.w600,
-                fontSize: 14,
-                //Commented out because of occuring bug
-                //height: 20,
-              ),
-              bodyLarge: const TextStyle(
-                fontFamily: "Noto Sans",
-                fontWeight: FontWeight.w500,
-                fontSize: 16,
-                //height: 20,
-              ),
-              bodyMedium: const TextStyle(
-                fontFamily: "Noto Sans",
-                fontWeight: FontWeight.w400,
-                fontSize: 14,
-                //height: 20,
-              ),
-              bodySmall: const TextStyle(
-                fontFamily: "Noto Sans",
-                fontWeight: FontWeight.w400,
-                fontSize: 12,
-                //height: 16,
-              ),
-            ),
-      ),
-      home: const PageWrapper(),
+    return DynamicColorBuilder(
+      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        ColorScheme lightScheme;
+        ColorScheme darkScheme;
+
+        if (lightDynamic != null && darkDynamic != null) {
+          lightScheme = lightDynamic.harmonized();
+          lightCustomColors = lightCustomColors.harmonized(lightScheme);
+
+          // Repeat for the dark color scheme.
+          darkScheme = darkDynamic.harmonized();
+          darkCustomColors = darkCustomColors.harmonized(darkScheme);
+        } else {
+          // Otherwise, use fallback schemes.
+          lightScheme = lightColorScheme;
+          darkScheme = darkColorScheme;
+        }
+
+        return MaterialApp(
+          title: 'Vocabulary',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: lightScheme,
+            extensions: [lightCustomColors],
+            textTheme: Theme.of(context).textTheme.copyWith(
+                  displaySmall: const TextStyle(
+                    fontFamily: "Catamaran",
+                    fontSize: 28,
+                    fontWeight: FontWeight.w700,
+                    //height: 44,
+                  ),
+                  headlineMedium: const TextStyle(
+                    fontFamily: "Catamaran",
+                    fontSize: 28,
+                    fontWeight: FontWeight.w700,
+                    //height: 36,
+                  ),
+                  headlineSmall: const TextStyle(
+                    fontFamily: "Catamaran",
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                    //height: 32,
+                  ),
+                  titleLarge: const TextStyle(
+                    fontFamily: "Catamaran",
+                    fontWeight: FontWeight.w600,
+                    fontSize: 22,
+                    //height: 28,
+                  ),
+                  labelLarge: const TextStyle(
+                    fontFamily: "Noto Sans",
+                    fontWeight: FontWeight.w600,
+                    fontSize: 14,
+                    //Commented out because of occuring bug
+                    //height: 20,
+                  ),
+                  bodyLarge: const TextStyle(
+                    fontFamily: "Noto Sans",
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16,
+                    //height: 20,
+                  ),
+                  bodyMedium: const TextStyle(
+                    fontFamily: "Noto Sans",
+                    fontWeight: FontWeight.w400,
+                    fontSize: 14,
+                    //height: 20,
+                  ),
+                  bodySmall: const TextStyle(
+                    fontFamily: "Noto Sans",
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12,
+                    //height: 16,
+                  ),
+                ),
+          ),
+          darkTheme: ThemeData(
+            useMaterial3: true,
+            colorScheme: darkScheme,
+            extensions: [darkCustomColors],
+            textTheme: Theme.of(context).textTheme.copyWith(
+                  displaySmall: const TextStyle(
+                    fontFamily: "Catamaran",
+                    fontSize: 28,
+                    fontWeight: FontWeight.w700,
+                    //height: 44,
+                  ),
+                  headlineMedium: const TextStyle(
+                    fontFamily: "Catamaran",
+                    fontSize: 28,
+                    fontWeight: FontWeight.w700,
+                    //height: 36,
+                  ),
+                  headlineSmall: const TextStyle(
+                    fontFamily: "Catamaran",
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                    //height: 32,
+                  ),
+                  titleLarge: const TextStyle(
+                    fontFamily: "Catamaran",
+                    fontWeight: FontWeight.w600,
+                    fontSize: 22,
+                    //height: 28,
+                  ),
+                  labelLarge: const TextStyle(
+                    fontFamily: "Noto Sans",
+                    fontWeight: FontWeight.w600,
+                    fontSize: 14,
+                    //Commented out because of occuring bug
+                    //height: 20,
+                  ),
+                  bodyLarge: const TextStyle(
+                    fontFamily: "Noto Sans",
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16,
+                    //height: 20,
+                  ),
+                  bodyMedium: const TextStyle(
+                    fontFamily: "Noto Sans",
+                    fontWeight: FontWeight.w400,
+                    fontSize: 14,
+                    //height: 20,
+                  ),
+                  bodySmall: const TextStyle(
+                    fontFamily: "Noto Sans",
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12,
+                    //height: 16,
+                  ),
+                ),
+          ),
+          home: const PageWrapper(),
+        );
+      },
     );
   }
 }
