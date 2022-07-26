53331d362087530ee1843cde912420e6ad294dc6
Dominik Schulz <dominikschulz@gmx.net>
2022-07-26 11:49:33 +0200

    Fixed button positioning and icon size

--- lib/screens/training/test_results/test_result_0.dart
+++ lib/screens/training/test_results/test_result_0.dart
@@ -15,127 +15,133 @@ class TestResult0Screen extends StatelessWidget {
           extendBodyBehindAppBar: true,
           body: Stack(
             children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: constraints.maxHeight * 0.5,
-                    child: Padding(
-                      padding:
-                          EdgeInsets.only(top: constraints.maxHeight * 0.1),
-                      child: Text(
-                        "Your Result",
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineMedium!
-                            .copyWith(
-                                color: Theme.of(context).colorScheme.secondary),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: constraints.maxHeight * 0.5,
-                    child: Column(
-                      children: [
-                        Text(
-                          "Keep learning!",
-                          style: Theme.of(context)
-                              .textTheme
-                              .titleLarge!
-                              .copyWith(
-                                  color:
-                                      Theme.of(context).colorScheme.secondary),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            children: [
-                              Text(
-                                " 0 ",
-                                style: AdditionalFonts.points.copyWith(
+              Center(
+                child: SizedBox(
+                  height: constraints.maxHeight,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: constraints.maxHeight * 0.5,
+                        child: Padding(
+                          padding:
+                              EdgeInsets.only(top: constraints.maxHeight * 0.1),
+                          child: Text(
+                            "Your Result",
+                            style: Theme.of(context)
+                                .textTheme
+                                .headlineMedium!
+                                .copyWith(
                                     color: Theme.of(context)
                                         .colorScheme
                                         .secondary),
-                              ),
-                              Icon(Icons.auto_awesome,
-                                  color:
-                                      Theme.of(context).colorScheme.secondary),
-                            ],
-                            mainAxisAlignment: MainAxisAlignment.center,
                           ),
                         ),
-                        FractionallySizedBox(
-                          widthFactor: 0.85,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              const Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 8.0),
-                                child: const LevelUpsCard(
-                                  amount: 0,
-                                ),
-                              ),
-                              const Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 8.0),
-                                child: const LevelDownsCard(
-                                  amount: 100,
-                                ),
-                              ),
-                              SizedBox(
-                                  height: MediaQuery.of(context).size.height *
-                                      0.075),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
+                      ),
+                      SizedBox(
+                        height: constraints.maxHeight * 0.5,
+                        width: constraints.maxWidth * 0.85,
+                        child: Column(
+                          children: [
+                            Text(
+                              "Keep learning!",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleLarge!
+                                  .copyWith(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .secondary),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
                                 children: [
-                                  ElevatedButton.icon(
-                                    onPressed: () {},
-                                    icon: Icon(Icons.replay_rounded,
+                                  Text(
+                                    " 0 ",
+                                    style: AdditionalFonts.points.copyWith(
                                         color: Theme.of(context)
                                             .colorScheme
-                                            .onSecondaryContainer),
-                                    label: Text(
-                                      "Retry",
-                                      style: TextStyle(
-                                          color: Theme.of(context)
-                                              .colorScheme
-                                              .onSecondaryContainer),
-                                    ),
-                                    style: ButtonStyle(
-                                        backgroundColor:
-                                            MaterialStateProperty.all(
-                                                Theme.of(context)
-                                                    .colorScheme
-                                                    .secondaryContainer)),
+                                            .secondary),
                                   ),
-                                  ElevatedButton(
-                                    onPressed: () {},
-                                    child: Text(
-                                      "Continue",
-                                      style: TextStyle(
-                                          color: Theme.of(context)
-                                              .colorScheme
-                                              .onPrimary),
-                                    ),
-                                    style: ButtonStyle(
-                                      backgroundColor:
-                                          MaterialStateProperty.all(
-                                              Theme.of(context)
-                                                  .colorScheme
-                                                  .secondary),
-                                    ),
-                                  )
+                                  Icon(Icons.auto_awesome,
+                                      size: 36,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .secondary),
                                 ],
-                              )
-                            ],
-                          ),
+                                mainAxisAlignment: MainAxisAlignment.center,
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.symmetric(vertical: 8.0),
+                              child: LevelUpsCard(
+                                amount: 0,
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.symmetric(vertical: 8.0),
+                              child: LevelDownsCard(
+                                amount: 100,
+                              ),
+                            ),
+                            Expanded(
+                              child: Align(
+                                alignment: FractionalOffset.bottomCenter,
+                                child: Padding(
+                                  padding: const EdgeInsets.only(bottom: 48.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      ElevatedButton.icon(
+                                        onPressed: () {},
+                                        icon: Icon(Icons.replay_rounded,
+                                            color: Theme.of(context)
+                                                .colorScheme
+                                                .onSecondaryContainer),
+                                        label: Text(
+                                          "Retry",
+                                          style: TextStyle(
+                                              color: Theme.of(context)
+                                                  .colorScheme
+                                                  .onSecondaryContainer),
+                                        ),
+                                        style: ButtonStyle(
+                                            backgroundColor:
+                                                MaterialStateProperty.all(
+                                                    Theme.of(context)
+                                                        .colorScheme
+                                                        .secondaryContainer)),
+                                      ),
+                                      ElevatedButton(
+                                        onPressed: () {},
+                                        child: Text(
+                                          "Continue",
+                                          style: TextStyle(
+                                              color: Theme.of(context)
+                                                  .colorScheme
+                                                  .onPrimary),
+                                        ),
+                                        style: ButtonStyle(
+                                          backgroundColor:
+                                              MaterialStateProperty.all(
+                                                  Theme.of(context)
+                                                      .colorScheme
+                                                      .secondary),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
               SizedBox(
                 width: constraints.maxWidth,
