c482e0c9ce17058415c1ce42566967c200dab61e
Dominik Schulz <dominikschulz@gmx.net>
2022-07-31 15:18:10 +0200

    Added first buttons to dashboard

---
+++ lib/components/buttons/dashboard/vocab_dashboard_vocabulary_button.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:vocabulary/styles/custom_color.g.dart';
+
+class VocabDashboardVocabularyButton extends StatelessWidget {
+  const VocabDashboardVocabularyButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return SizedBox.fromSize(
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          SvgPicture.asset(
+            'images/dashboard/vocabulary-btn.svg',
+            color: CustomGreen,
+            width: size.width * 0.65,
+          ),
+          Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: Icon(
+                  Icons.translate,
+                  size: 48,
+                ),
+              ),
+              Text(
+                "5432100",
+                style: Theme.of(context).textTheme.labelLarge,
+              ),
+              Text(
+                "Vocabulary items",
+                style: Theme.of(context).textTheme.labelLarge,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:vocabulary/components/buttons/dashboard/vocab_dashboard_vocabulary_button.dart';
+import 'package:vocabulary/styles/custom_color.g.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Dashboard"),
+    final Size size = MediaQuery.of(context).size;
+
+    return Stack(
+      clipBehavior: Clip.hardEdge,
+      children: [
+        Positioned(
+          left: -30,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: size.height * 0.125,
+              ),
+              const VocabDashboardVocabularyButton()
+            ],
+          ),
+        ),
+        Positioned(
+          right: -30,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: size.height * 0.25,
+              ),
+              const VocabDashboardVocabularyButton()
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: size.height * 0.05),
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: Text(
+              "Welcome back!",
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineMedium!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
