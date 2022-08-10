be979bdd08977698261f1e8b584cefcd58b2932b
Dominik Schulz <dominikschulz@gmx.net>
2022-08-10 13:13:57 +0200

    Added vocabulary service and json model as well as an app-wide provider

--- lib/models/vocabulary.dart
+++ lib/models/vocabulary.dart
@@ -1,19 +1,29 @@
-class Vocabulary {
-  List<String> nativeLanguageMeanings;
-  List<String> newLanguageMeanings;
-  String language;
-  List<String> tags;
-  String additionalInformation;
-  int level;
+import 'package:json_annotation/json_annotation.dart';
 
-  Vocabulary(
-    this.nativeLanguageMeanings,
-    this.newLanguageMeanings,
-    this.language,
-    this.tags,
-    this.additionalInformation,
-    this.level,
-  );
+part 'vocabulary.g.dart';
+
+@JsonSerializable()
+class Vocabulary {
+  final List<String> nativeLanguageMeanings;
+  final List<String> newLanguageMeanings;
+  final String language;
+  final List<String> tags;
+  final String additionalInformation;
+  final int level;
+
+  Vocabulary({
+    required this.nativeLanguageMeanings,
+    required this.newLanguageMeanings,
+    required this.language,
+    required this.tags,
+    required this.additionalInformation,
+    required this.level,
+  });
+
+  factory Vocabulary.fromJson(Map<String, dynamic> json) =>
+      _$VocabularyFromJson(json);
+
+  Map<String, dynamic> toJson() => _$VocabularyToJson(this);
 
   @override
   String toString() {
