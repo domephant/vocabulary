2cb26cd26bdb0a1c4cf81f283539eb1ff094ffd9
Dominik Schulz <dominikschulz@gmx.net>
2022-08-14 22:06:24 +0200

    Fixed dropdownbutton and scroll behavoir

--- lib/components/buttons/new_vocabulary/vocab_language_dropdown_button.dart
+++ lib/components/buttons/new_vocabulary/vocab_language_dropdown_button.dart
@@ -18,21 +18,24 @@ class _VocabLanguageDropdownButtonState
   @override
   Widget build(BuildContext context) {
     return DropdownButtonHideUnderline(
-      child: DropdownButton(
-        borderRadius: BorderRadius.circular(15),
-        value: dropdownValue,
-        isExpanded: true,
-        items: widget.items
-            .map((String value) => DropdownMenuItem(
-                  child: Text(value),
-                  value: value,
-                ))
-            .toList(),
-        onChanged: (String? newValue) {
-          setState(() {
-            dropdownValue = newValue!;
-          });
-        },
+      child: ButtonTheme(
+        alignedDropdown: true,
+        child: DropdownButton(
+          borderRadius: BorderRadius.circular(8),
+          value: dropdownValue,
+          isExpanded: true,
+          items: widget.items
+              .map((String value) => DropdownMenuItem(
+                    child: Text(value),
+                    value: value,
+                  ))
+              .toList(),
+          onChanged: (String? newValue) {
+            setState(() {
+              dropdownValue = newValue!;
+            });
+          },
+        ),
       ),
     );
   }
--- lib/screens/new_vocabulary.dart
+++ lib/screens/new_vocabulary.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:vocabulary/components/buttons/new_vocabulary/vocab_add_meaning_button.dart';
-import 'package:vocabulary/components/display/new_vocabulary/vocab_chip.dart';
 import 'package:vocabulary/components/display/new_vocabulary/vocab_left_aligned_text.dart';
 import 'package:vocabulary/components/inputs/new_vocabulary/vocab_input_column.dart';
 import 'package:vocabulary/components/buttons/new_vocabulary/vocab_language_dropdown_button.dart';
@@ -17,63 +15,71 @@ class NewVocabularyScreen extends StatelessWidget {
     final List<String> values = ['Test1', 'Test2', 'Test3'];
 
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: const VocabAppBar(
         profile: false,
         title: Text("New Vocabulary"),
         saveButton: true,
       ),
-      body: Center(
-        child: FractionallySizedBox(
-          heightFactor: 1,
-          widthFactor: 0.85,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              VocabLeftAlignedText(
-                text: 'Your Language',
-                style: Theme.of(context).textTheme.bodySmall,
-              ),
-              const VocabInputColumn(),
-              VocabLeftAlignedText(
-                text: 'Other Language',
-                style: Theme.of(context).textTheme.bodySmall,
-              ),
-              const VocabInputColumn(),
-              VocabLeftAlignedText(
-                text: 'Vocabulary Language',
-                style: Theme.of(context).textTheme.bodySmall,
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 8.0, 0, 16.0),
-                child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    border: Border.all(
-                        color: Theme.of(context).colorScheme.outline),
-                  ),
-                  child: VocabLanguageDropdownButton(items: values),
+      body: LayoutBuilder(
+        builder: (context, constraints) => SizedBox(
+          width: constraints.maxWidth,
+          height: constraints.maxHeight,
+          child: Center(
+            child: FractionallySizedBox(
+              heightFactor: 1,
+              widthFactor: 0.85,
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    VocabLeftAlignedText(
+                      text: 'Your Language',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                    const VocabInputColumn(),
+                    VocabLeftAlignedText(
+                      text: 'Other Language',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                    const VocabInputColumn(),
+                    VocabLeftAlignedText(
+                      text: 'Vocabulary Language',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 8.0, 0, 16.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          border: Border.all(
+                              color: Theme.of(context).colorScheme.outline),
+                        ),
+                        child: VocabLanguageDropdownButton(items: values),
+                      ),
+                    ),
+                    VocabLeftAlignedText(
+                      text: 'Tags',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    VocabTagInput(),
+                    VocabLeftAlignedText(
+                      text: 'Additional Information',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    TextField(
+                      controller: _additionalInformationController,
+                      decoration: InputDecoration(
+                        focusColor: Theme.of(context).colorScheme.onSurface,
+                        fillColor: Theme.of(context).colorScheme.onSurface,
+                        border: const UnderlineInputBorder(),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              VocabLeftAlignedText(
-                text: 'Tags',
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              VocabTagInput(),
-              VocabLeftAlignedText(
-                text: 'Additional Information',
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              TextField(
-                controller: _additionalInformationController,
-                decoration: InputDecoration(
-                  focusColor: Theme.of(context).colorScheme.onSurface,
-                  fillColor: Theme.of(context).colorScheme.onSurface,
-                  border: const UnderlineInputBorder(),
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
