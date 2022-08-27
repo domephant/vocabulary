7ca1ac64dc46b55d589644106ec6e84a98fbf9f0
Dominik Schulz <dominikschulz@gmx.net>
2022-08-27 23:26:09 +0200

    Added parameters to chip

--- lib/components/display/new_vocabulary/vocab_chip.dart
+++ lib/components/display/new_vocabulary/vocab_chip.dart
@@ -3,8 +3,15 @@ import 'package:flutter/material.dart';
 class VocabChip extends StatelessWidget {
   final String? title;
   final Color? backgroundColor;
-  const VocabChip({Key? key, this.title, this.backgroundColor})
-      : super(key: key);
+  final int? index;
+  final VoidCallback? onDeleted;
+  const VocabChip({
+    Key? key,
+    this.title,
+    this.backgroundColor,
+    this.onDeleted,
+    this.index,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +25,7 @@ class VocabChip extends StatelessWidget {
         size: 20,
       ),
       backgroundColor: backgroundColor,
-      onDeleted: () {},
+      onDeleted: onDeleted,
     );
   }
 }
