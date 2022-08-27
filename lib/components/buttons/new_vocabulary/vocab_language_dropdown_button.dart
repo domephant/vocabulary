0cf78ad851eedc8bb48f7deaad2c0ebe042a95e8
Dominik Schulz <dominikschulz@gmx.net>
2022-08-27 23:26:34 +0200

    Changed static dropdown to async dropdown from database

--- lib/components/buttons/new_vocabulary/vocab_language_dropdown_button.dart
+++ lib/components/buttons/new_vocabulary/vocab_language_dropdown_button.dart
@@ -1,10 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:vocabulary/services/authentication_service.dart';
+import 'package:vocabulary/services/database_service.dart';
 
 class VocabLanguageDropdownButton extends StatefulWidget {
-  final List<String> items;
-
-  const VocabLanguageDropdownButton({Key? key, required this.items})
-      : super(key: key);
+  const VocabLanguageDropdownButton({Key? key}) : super(key: key);
 
   @override
   State<VocabLanguageDropdownButton> createState() =>
@@ -13,31 +14,68 @@ class VocabLanguageDropdownButton extends StatefulWidget {
 
 class _VocabLanguageDropdownButtonState
     extends State<VocabLanguageDropdownButton> {
-  late String dropdownValue = widget.items[0];
+  var dropdownValue;
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButtonHideUnderline(
-      child: ButtonTheme(
-        alignedDropdown: true,
-        child: DropdownButton(
-          borderRadius: BorderRadius.circular(8),
-          value: dropdownValue,
-          style: Theme.of(context).textTheme.bodyLarge,
-          isExpanded: true,
-          items: widget.items
-              .map((String value) => DropdownMenuItem(
-                    child: Text(value),
-                    value: value,
-                  ))
-              .toList(),
-          onChanged: (String? newValue) {
-            setState(() {
-              dropdownValue = newValue!;
-            });
-          },
-        ),
-      ),
+    return FutureBuilder<QuerySnapshot>(
+      future: Provider.of<DatabaseService>(context, listen: false)
+          .getAllMotherTongues(context.read<AuthenticationService>()),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<String> languages =
+              snapshot.data!.docs.map((e) => e.get('name').toString()).toList();
+
+          return DropdownButtonHideUnderline(
+            child: ButtonTheme(
+              alignedDropdown: true,
+              child: DropdownButton(
+                hint: const Text('Select a language'),
+                borderRadius: BorderRadius.circular(8),
+                value: dropdownValue,
+                style: Theme.of(context).textTheme.bodyLarge,
+                isExpanded: true,
+                items: languages
+                    .map((String value) => DropdownMenuItem(
+                          child: Text(value),
+                          value: value,
+                        ))
+                    .toList(),
+                onChanged: (Object? newValue) {
+                  setState(() {
+                    dropdownValue = newValue!;
+                  });
+                },
+              ),
+            ),
+          );
+        } else {
+          String dropdownValue = 'No Data Found';
+          DropdownButtonHideUnderline(
+            child: ButtonTheme(
+              alignedDropdown: true,
+              child: DropdownButton(
+                borderRadius: BorderRadius.circular(8),
+                value: dropdownValue,
+                style: Theme.of(context).textTheme.bodyLarge,
+                isExpanded: true,
+                items: const [
+                  DropdownMenuItem(
+                    child: Text('No Data Found'),
+                    value: 'No Data Found',
+                  ),
+                ].toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownValue = newValue!;
+                  });
+                },
+              ),
+            ),
+          );
+        }
+        return const CircularProgressIndicator();
+      },
     );
   }
 }
